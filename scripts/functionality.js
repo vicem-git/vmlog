45aa3deaeea4ea2b81a84658ce945ea8bb5edc62
vicem-git <macellariv@gmail.com>
2024-04-30 11:17:38 +0200

    debugging

--- scripts/functionality.js
+++ scripts/functionality.js
@@ -1,6 +1,13 @@
 // FUNCTIONS
 
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('.port-butt').addEventListener('click', function() {
+       document.querySelector('.drop-content').classList.toggle('show');
+    });
+   });
+   
+
 //  toggle dropdown
-function toggleDropdown() {
-    document.getElementById("port-dropdown").classList.toggle("show");
-   }
+//  function toggleDropdown() {
+//        document.getElementById("port-dropdown").classList.toggle("show");
+//  }
