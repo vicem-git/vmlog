512e02e32b7bc2858b09c69a70b6ff0c39581c76
vicem-git <macellariv@gmail.com>
2024-05-01 09:38:20 +0200

    media query fix

--- scripts/functionality.js
+++ scripts/functionality.js
@@ -1,13 +1,24 @@
 // FUNCTIONS
 
+//  toggle dropdown
+
+function toggleDropdown() {
+   var dropdown = this.nextElementSibling;
+   if (dropdown.style.display === 'none') {
+       dropdown.style.display = 'block';
+   } else {
+       dropdown.style.display = 'none';
+   }
+}
+
+
 document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('.port-butt').addEventListener('click', function() {
-       document.querySelector('.drop-content').classList.toggle('show');
-    });
-   });
+   document.querySelector('.port-butt').addEventListener('click', toggleDropdown);
+   document.querySelector('.port-butt').addEventListener('touchstart', toggleDropdown);
+});
+
    
 
-//  toggle dropdown
-//  function toggleDropdown() {
-//        document.getElementById("port-dropdown").classList.toggle("show");
-//  }
+
+
+
