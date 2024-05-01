fa5291c942d5c96b6ee762cd12817ef115fcc7a0
vicem-git <macellariv@gmail.com>
2024-05-01 14:40:28 +0200

    fix big

--- scripts/functionality.js
+++ scripts/functionality.js
@@ -1,21 +1,17 @@
 // FUNCTIONS
 
 //  toggle dropdown
+document.querySelector('.port-butt').addEventListener('click', toggleDropdown);
+document.querySelector('.port-butt').addEventListener('touchstart', toggleDropdown);
 
 function toggleDropdown() {
-   var dropdown = this.nextElementSibling;
-   if (dropdown.style.display === 'none') {
-       dropdown.style.display = 'block';
-   } else {
-       dropdown.style.display = 'none';
-   }
-}
-
-
-document.addEventListener('DOMContentLoaded', function() {
-   document.querySelector('.port-butt').addEventListener('click', toggleDropdown);
-   document.querySelector('.port-butt').addEventListener('touchstart', toggleDropdown);
-});
+    var dropdown = this.nextElementSibling;
+    if (dropdown.style.display === 'none') {
+        dropdown.style.display = 'block';
+    } else {
+        dropdown.style.display = 'none';
+    }
+ }
 
    
 
